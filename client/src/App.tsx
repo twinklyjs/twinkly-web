058d9d04b74cd9dab64bbb8b59583e7e1c97df31
Justin Beckwith <justin.beckwith@gmail.com>
2025-01-04 21:17:27 -0800

    bump twinklyjs and apply broader linting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,48 @@
-import { useState, useEffect } from 'react'
 import {api} from '@twinklyjs/twinkly';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import viteLogo from '/vite.svg';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
+	const [summary, setSummary] = useState('');
 
-  useEffect(() => {
-    (async () => {
-      api.init('localhost:3000', {additionalHeaders: { 'x-twinkly-ip': '10.0.0.167' }});
-      const res = await api.echo({message: 'Hello World!'});
-      console.log(res);
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			api.init('localhost:3000', {
+				additionalHeaders: { 'x-twinkly-ip': '10.0.0.167' },
+			});
+			const res = await api.getSummary();
+			setSummary(JSON.stringify(res, null, 2));
+		})();
+	}, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<a href="https://vite.dev" target="_blank" rel="noreferrer">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank" rel="noreferrer">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Twinklyjs ON THE WEB</h1>
+			<div className="card">
+				<pre className="summary">{summary}</pre>
+				<button type="button" onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	);
 }
 
-export default App
+export default App;
